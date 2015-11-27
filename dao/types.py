ff7cb31b953f1ea380e35e978799a3a2387b6855
Joey Tuong <joey@tetrap.us>
2015-11-28 03:00:36 +1100

    Add description and time fields
    [#109056132]

--- dao/types.py
+++ dao/types.py
@@ -1,7 +1,15 @@
+from calendar import timegm
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.orderinglist import ordering_list
 
 Base = declarative_base()
+
+def epoch_of(timestamp):
+    if timestamp is not None:
+        return timegm(timestamp.timetuple())
 
 
 class User(Base):
@@ -23,6 +31,8 @@ class TaskStatus(object):
     ACTIVE = 0
     DONE = 1
     DELETED = 2
+    PENDING = 3
+    EXPIRED = 4 # Expired is a variant of active, with the same semantics.
 
 class Task(Base):
     __tablename__ = 'tasks'
@@ -34,6 +44,13 @@ class Task(Base):
     points = Column(Integer)
     status = Column(Integer)
     idx = Column(Integer)
+    description = Column(String)
+
+    created = Column(DateTime)
+    activates = Column(DateTime)
+    expires = Column(DateTime)
+
+    subtasks = relationship("Task", order_by="Task.idx", collection_class=ordering_list('idx'))
 
     def to_dict(self):
         return {
@@ -44,5 +61,10 @@ class Task(Base):
             "title": self.title,
             "points": self.points,
             "status": self.status,
-            "idx": self.idx
+            "idx": self.idx,
+            "description": self.description,
+            "subtasks": [i.to_dict() for i in self.subtasks],
+            "created": epoch_of(self.created),
+            "activates": epoch_of(self.created),
+            "expires": epoch_of(self.created)
         }
