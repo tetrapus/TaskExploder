4c87854348b0fc7229215783b72a08e223eebf94
Joey Tuong <joey@tetrap.us>
2016-05-15 19:33:34 +1000

    Code cleanup

--- taskexploder/api/accounts.py
+++ taskexploder/api/accounts.py
@@ -1,3 +1,4 @@
+''' API methods related to user accounts '''
 import uuid
 
 from flask import g, request, jsonify, session, abort
@@ -5,7 +6,8 @@ from flask import g, request, jsonify, session, abort
 from ..model.users import User, hash_pwd
 
 
-def create_user(session, username, password):
+def create_user(database, username, password):
+    ''' Insert a user into the database '''
     # TODO: Check username is unique
     # Generate a salt
     salt = uuid.uuid4().hex
@@ -16,36 +18,46 @@ def create_user(session, username, password):
         password=password_hash,
         salt=salt
     )
-    session.add(user)
-    session.commit()
+    database.add(user)
+    database.commit()
 
     return user
 
 
+def create_account():
+    ''' Register a new user '''
+    account = create_user(g.db, request.form['username'], request.form['password'])
+    return jsonify({"status": "success", "data": account.to_dict()})
+
+
+def get_account():
+    ''' Get a user object by username '''
+    account = User.from_username(g.db, request.args['username'])
+    if account is None:
+        return jsonify({
+            'status': 'error',
+            'message': 'User does not exist.'
+        })
+    return jsonify({
+        "status": "success",
+        "data": account.to_dict()
+    })
+
+
+def get_current_user():
+    ''' Return the user object associate with the current session '''
+    if not session.get('user'):
+        abort(401)
+    return jsonify({
+        'status': 'success',
+        'data': session.get('user'),
+    })
+
+
 def register(blueprint):
-    @blueprint.route('/account', methods=['POST'])
-    def create_account():
-        account = create_user(g.db, request.form['username'], request.form['password'])
-        return jsonify({"status": "success", "data": account.to_dict()})
+    ''' Register account endpoints '''
+    blueprint.route('/account', methods=['POST'])(create_account)
 
-    @blueprint.route('/account', methods=['GET'])
-    def get_account():
-        account = User.from_username(g.db, request.args['username'])
-        if account is None:
-            return jsonify({
-                'status': 'error',
-                'message': 'User does not exist.'
-            })
-        return jsonify({
-            "status": "success",
-            "data": account.to_dict()
-        })
+    blueprint.route('/account', methods=['GET'])(get_account)
 
-    @blueprint.route("/whoami", methods=["GET"])
-    def get_current_user():
-        if not session.get('user'):
-            abort(401)
-        return jsonify({
-            'status': 'success',
-            'data': session.get('user'),
-        })
+    blueprint.route("/whoami", methods=["GET"])(get_current_user)
